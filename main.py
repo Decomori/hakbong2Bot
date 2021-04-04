e43933e47267206e09641f4c79497e6a4eb52408
김태성 <samjo_v@gimtaeseong-ui-MacBookPro.local>
2021-04-04 19:13:29 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!박경배 ")
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def 훈련병(ctx):
+    await ctx.send('128번 훈련병 박경배!')
+
+
+@bot.command()
+async def 계급(ctx):
+    await ctx.send('현재 저는 훈련병입니다!')
+
+
+bot.run('ODI3OTQ3NDIyNTc1MzYyMDQ4.YGicIA.npgkNgDtKl4vlK4sjvPJm0pzGpQ')
